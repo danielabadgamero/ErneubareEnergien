cf9c455fe81405c179e9dbce3e4f8761e2ca535f
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-26 14:12:15 +0200

    add nav.

---
+++ src/script.js
@@ -0,0 +1,10 @@
+document.getElementById("home").onclick = () => {
+    window.location.href = "/";
+}
+
+let nav = document.getElementById("nav");
+nav.childNodes.forEach((child) => {
+    child.addEventListener("click", () => {
+
+    })
+})
