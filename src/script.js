3b11ba774ef6ec1d91ec60d38bd22c2001eae6fb
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-27 09:19:13 +0200

    Add more html pages.

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 document.getElementById("home").onclick = () => {
-    window.location.href = "/";
+	window.location.href = "/";
 }
 
 let nav = document.getElementById("nav");
 nav.childNodes.forEach((child) => {
-    child.addEventListener("click", () => {
-        if (child.id == "types") {
+	child.addEventListener("click", () => {
+		if (child.id == "types") {
 
-        }
-        else
-            window.location.href = child.id + ".html";
-    })
+		}
+		else
+			window.location.href = child.id + ".html";
+	})
 })
