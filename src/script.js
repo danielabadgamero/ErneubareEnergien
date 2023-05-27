7c84a56f516e08c854eb881a6d0c489192d3e0d3
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-27 09:45:03 +0200

    Move type pages outside of the folder.

--- src/script.js
+++ src/script.js
@@ -5,10 +5,15 @@ document.getElementById("home").onclick = () => {
 let nav = document.getElementById("nav");
 nav.childNodes.forEach((child) => {
 	child.addEventListener("click", () => {
-		if (child.id == "types") {
-
-		}
-		else
+		if (child.id != "types") {
 			window.location.href = child.id + ".html";
+		}
 	})
 })
+
+let list = document.getElementById("arten-list");
+list.childNodes.forEach((type) => {
+	type.addEventListener("click", () => {
+		window.location.href = type.id + ".html";
+	});
+});
